4b9e65a3ef14d0b3bbc315e09ad250ea5b2f4bae
Naidupoluparthi <naidupoluparthi888@gmail.com>
2025-09-14 11:46:47 +0530

    initial commit

---
+++ home.js
@@ -0,0 +1,31 @@
+function ContactFormEnter() {
+    document.getElementById('contactButton').style.backgroundColor = 'blue';
+    document.getElementById('contactButton').style.color = 'white';
+    document.getElementById('contactButton').style.transform = 'scale(1.1)';
+}
+
+function ContactFormLeave() {
+    document.getElementById('contactButton').style.backgroundColor = 'white';
+    document.getElementById('contactButton').style.color = 'rgb(32, 33, 54)';
+    document.getElementById('contactButton').style.transform = 'scale(1)';
+}
+function ContactFormEnter2() {
+    document.getElementById('zoom').style.backgroundColor = 'blue';
+    document.getElementById('zoom').style.color = 'white';
+    document.getElementById('zoom').style.transform = 'scale(1.1)';
+}
+function ContactFormLeave2() {
+    document.getElementById('zoom').style.backgroundColor = 'white';
+    document.getElementById('zoom').style.color = 'rgb(32, 33, 54)';
+    document.getElementById('zoom').style.transform = 'scale(1)';
+}
+function formbuttonenter() {
+    document.getElementById('form').style.backgroundColor = 'rgba(100, 55, 188, 0.85)';
+    document.getElementById('form').style.color = 'white';
+    document.getElementById('form').style.transform = 'scale(1.1)';
+}
+function formbuttonleave() {
+    document.getElementById('form').style.backgroundColor = 'rgba(96, 102, 220, 1)';
+    document.getElementById('form').style.color = 'white';
+    document.getElementById('form').style.transform = 'scale(1)';
+}
